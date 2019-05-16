a927fe7bfb3321d098748d015bb0d2ae6511de9a
Harsh Patel <harshaudi27@gmail.com>
2019-05-16 20:26:08 +0530

    Added imports to app

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from flask import Flask
+from textwrap import dedent
+from uuid import uuid4
+import json
+
+from flask import Flask, jsonify, request
+
+from Blockchain import Blockchain
 
 # Instantiates the node
 app = Flask(__name__)
@@ -6,7 +12,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "App is deployed"
+    return "App is deployed and Imports are ready"
 
 if __name__ == "__main__":
     app.run()
